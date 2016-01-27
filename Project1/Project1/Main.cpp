33e9eb16da75995a3422ffb5e5ea4a5f5d95a3fe
BuildTools <unconfigured@null.spigotmc.org>
2016-01-27 14:16:56 -0600

    Reverted the class based framework. Now have fully functional Car Menu function and Credit Score function Still need to add compound interest equation to find monthly payment

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -2,26 +2,62 @@
 #include <string>
 using namespace std;
 
-class Car {
-public:
-    string make;
-    int price;
+
+double CreditScore(int score) {
+    if (score <= 500) {
+        return 0.05;
+    }
+    else if (score > 500 && score < 700) {
+        return 0.02;
+    }
+    else if (score >= 700) {
+        return 0.01;
+    }
+}
 
 
-};
+double Car_Menu() {
+    double Car1_Price, Car2_Price, Car3_Price;
+    string Car1, Car2, Car3;
+    Car1 = "Honda Civic";
+    Car2 = "Chrysler LeBaron";
+    Car3 = "Jeep Grand Cherokee";
+
+    Car1_Price = 10000.00;
+    Car2_Price = 15000.00;
+    Car3_Price = 20000.00;
+
+    cout << Car1 << " For $" << Car1_Price << " Use '1' to choose" << endl;
+    cout << Car2 << " For $" << Car2_Price << " Use '2' to choose" << endl;
+    cout << Car3 << " For $" << Car3_Price << " Use '3' to choose" << endl;
+
+    int user_choice;
+
+    cin >> user_choice;
+    if (user_choice == 1) {
+        cout << "You chose the " << Car1 << " For $" << Car1_Price << endl;
+        return Car1_Price;
+    }
+    else if (user_choice == 2) {
+        cout << "You chose the " << Car2 << " For $" << Car2_Price << endl;
+        return Car2_Price;
+    }
+    else if (user_choice == 3) {
+        cout << "You chose the " << Car3 << " For $" << Car3_Price << endl;
+        return Car3_Price;
+    }
+}
+
 
 int main() {
-    Car car1, car2, car3;
-    car1.make = "jeep";
-    car1.price = 1000;
-    car2.make = "lexus";
-    car2.price = 2000;
-    car3.make = "ford";
-    car3.price = 1500;
+    double cost = Car_Menu();
+    int credit_score;
+    cout << "What is your credit score? " << endl;
+    cin >> credit_score;
+    double interest_rate;
+    interest_rate = CreditScore(credit_score);
+    cout << "Your interest rate is: " << interest_rate << "%" << endl;
 
-    cout << "Car 1 is a " << car1.make << " Price of Car 1 is " << car1.price << endl;
-    cout << "Car 2 is a " << car2.make << " Price of Car 2 is " << car2.price << endl;
-    cout << "Car 3 is a " << car3.make << " Price of Car 3 is " << car3.price << endl;
 
     return 0;
 }
